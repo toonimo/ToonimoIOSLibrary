c9716010eed16db93fad2719a20ca2bd3bc4c653
Toonimo <roy@toonimo.com>
2025-05-21 14:08:27 +0300

    Release version 1.0.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .binaryTarget(
             name: "ToonimoIOSLibrary",
             url: "https://github.com/toonimo/ios/releases/download/1.0.0/ToonimoIOSLibrary.xcframework.zip",
-            checksum: ""
+            checksum: "265af77a4ed27cc30e3acd0c9bb8ad7e66c0943cb387f9db7e904b70fc6e5448"
         ),
     ]
 )
