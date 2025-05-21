f824a1cbe90b54a1fb16be614a28a6d0005f508d
AviramTMO <71327808+AviramTMO@users.noreply.github.com>
2025-05-21 17:18:26 +0300

    1.0.9

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ToonimoIOSLibrary",
-            url: "https://github.com/toonimo/ios/releases/download/1.0.8/ToonimoIOSLibraryFramework.xcframework.zip",
-            checksum: "ee3b68ddb76f4d770cd89e9e2e50d3f7a8cb514aa6fc482c339305239372dd2e"
+            url: "https://github.com/toonimo/ios/releases/download/1.0.9/ToonimoIOSLibraryFramework.xcframework.zip",
+            checksum: "5303869a733be62b437ff045a142912d85cc0912d7c18961ac618e5c4f96b520"
         ),
     ]
 )
